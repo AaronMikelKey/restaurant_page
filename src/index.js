69683dbcd94a2df476a51cedb48e75530e1c59fa
buckshot307 <buckshot307@gmail.com>
2020-10-02 00:00:39 -0400

    added bootstrap tabs, fixed CSS a bit.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import component from './initial.js';
 import './styles.css';
-// app.js
-import jTabs from 'jtabs';
-var container = document.querySelector('.tabs-container');
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-jTabs({
-    container      : container,
-    buttons        : container.querySelectorAll('.btn-js'),
-    tabs           : container.querySelectorAll('.tab-js'),
-    activeBtnClass : 'btn-active-js',
-    activeTabClass : 'tab-active-js'
-});
+
 
 document.getElementById('content').appendChild(component);
